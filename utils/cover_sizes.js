9a5556179a7ae0feed01fbb472a132d333dc50a5
Tema <kukukov.kuku@gmail.com>
2016-01-14 01:06:58 +0800

    обновил вспомогательные утилиты

---
+++ utils/cover_sizes.js
@@ -0,0 +1,36 @@
+(() => {
+    'use strict';
+
+    const MIN_SIZE = 10;
+    const MAX_SIZE = 2000;
+    const SIZE_INCREMENT = 5;
+    const coverUrl = 'https://avatars.yandex.net/get-music-content/78e6a9c4.a.960649-1/';
+    const options = {
+        method: 'HEAD',
+        mode: 'cors'
+    };
+
+    let chain = Promise.resolve();
+    let correctSizes = [];
+
+    console.log('Requesting...');
+
+    for (let i = MIN_SIZE; i <= MAX_SIZE; i += SIZE_INCREMENT) {
+        const size = `${i}x${i}`;
+        chain = chain.then(() => fetch(coverUrl + size, options))
+            .then(response => {
+                if (response.ok) {
+                    correctSizes.push(size);
+                }
+            }).catch(e => console.error(e));
+    }
+
+    chain.then(() => {
+        if (correctSizes.length) {
+            console.log('Sizes:', correctSizes);
+        } else {
+            console.log('No correct sizes were found');
+        }
+    });
+
+})();
--- utils/show_download_stats.js
+++ utils/show_download_stats.js
@@ -1,29 +1,35 @@
-/* global utils */
-
 (() => {
     'use strict';
 
-    const githubURL = 'https://api.github.com/repos/egoroof/yandex-music-fisher/releases';
-    const bitbucketURL = 'https://bitbucket.org/api/2.0/repositories/egoroof/yandex-music-fisher/downloads';
+    function parseResponse(response) {
+        if (!response.ok) {
+            throw new Error(`${response.status} (${response.statusText})`);
+        }
+        return response.json();
+    }
 
-    utils.ajax(githubURL, 'json').then(releases => {
-        console.log('GitHub statistics');
-        let totalDownloadCount = 0;
-        releases.forEach(release => {
-            console.log(release.name, release.assets[0].download_count);
-            totalDownloadCount += release.assets[0].download_count;
-        });
-        console.log('total', totalDownloadCount);
-    }).catch(error => console.error(error));
+    fetch('https://api.github.com/repos/egoroof/yandex-music-fisher/releases')
+        .then(parseResponse)
+        .then(releases => {
+            console.log('GitHub statistics');
+            let totalDownloadCount = 0;
+            releases.forEach(release => {
+                console.log(release.name, release.assets[0].download_count);
+                totalDownloadCount += release.assets[0].download_count;
+            });
+            console.log('total', totalDownloadCount);
+        }).catch(e => console.error(e));
 
-    utils.ajax(bitbucketURL, 'json').then(info => {
-        console.log('Bitbucket statistics');
-        let totalDownloadCount = 0;
-        info.values.forEach(download => {
-            console.log(download.name, download.downloads);
-            totalDownloadCount += download.downloads;
-        });
-        console.log('total', totalDownloadCount);
-    }).catch(error => console.error(error));
+    fetch('https://bitbucket.org/api/2.0/repositories/egoroof/yandex-music-fisher/downloads')
+        .then(parseResponse)
+        .then(info => {
+            console.log('Bitbucket statistics');
+            let totalDownloadCount = 0;
+            info.values.forEach(download => {
+                console.log(download.name, download.downloads);
+                totalDownloadCount += download.downloads;
+            });
+            console.log('total', totalDownloadCount);
+        }).catch(e => console.error(e));
 
 })();
