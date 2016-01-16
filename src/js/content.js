9fb3f8de59cfd2a3bfae77585c5e92d67b57c987
Tema <kukukov.kuku@gmail.com>
2016-01-16 23:08:08 +0800

    убран временный патч для радио

--- src/js/content.js
+++ src/js/content.js
@@ -3,16 +3,14 @@
 (() => {
     'use strict';
 
-    const IS_RADIO = location.hostname.match(/^radio\.yandex\.(ru|by|kz|ua)$/);
-
     const injectCode = func => {
         const script = document.createElement('script');
-        script.textContent = '"use strict";try {(' + func + ')(); } catch(e) {console.log("injected error", e);};';
-        (document.head || document.documentElement).appendChild(script);
+        script.textContent = `'use strict';try{(${func})();}catch(e){console.log('Fisher injected error',e);};`;
+        document.head.appendChild(script);
         script.parentNode.removeChild(script);
     };
 
-    const getCurrentTrackUrl = () => {
+    const dispatchCurrentTrackUrl = () => {
         let link;
         const track = externalAPI.getCurrentTrack();
         if (track && track.link) {
@@ -26,30 +24,11 @@
     };
 
     chrome.runtime.onMessage.addListener(message => {
-        // расширение прислало сообщение
         if (message === 'getCurrentTrackUrl') {
-            injectCode(getCurrentTrackUrl);
+            injectCode(dispatchCurrentTrackUrl);
         }
     });
 
-    // ловим события со страницы и отправляем расширению
     document.addEventListener('fisher_injected_event', e => chrome.runtime.sendMessage(e.detail));
-
-    if (IS_RADIO) {
-        injectCode(() => { // временный патч
-            var repo = Mu.blocks.di.repo;
-            repo.seq = repo.flow;
-
-            repo.seq.like = repo.seq.sendFeedback.bind(repo.seq, 'like', null, null);
-            repo.seq.unlike = repo.seq.sendFeedback.bind(repo.seq, 'unlike', null, null);
-            repo.seq.dislike = repo.seq.sendFeedback.bind(repo.seq, 'dislike', null, null);
-            repo.seq.undislike = repo.seq.sendFeedback.bind(repo.seq, 'undislike', null, null);
-
-            repo.fav.isFavorite = function (data) {
-                return data.entry.track.liked;
-            };
-            Mu.blocks.di.runInit('externalAPI', new Mu.ExternalAPI());
-        });
-    }
 
 })();
