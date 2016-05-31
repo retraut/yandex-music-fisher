5b9b12d1d77dad1a09650c2be04beb97522b18c5
Artyom Egorov <mail@egoroof.ru>
2016-05-31 12:51:42 +0800

    eslint-plugin-babel не нужен

--- tools/.eslintrc.js
+++ tools/.eslintrc.js
@@ -12,9 +12,6 @@ module.exports = {
         "PLATFORM_FIREFOX": false,
         "PLATFORM_CHROMIUM": false
     },
-    "plugins": [
-        "babel"
-    ],
     "extends": "eslint:recommended",
     "rules": { // http://eslint.org/docs/rules/
         "indent": 0,
@@ -27,15 +24,6 @@ module.exports = {
         "prefer-const": 2, // suggest using const declaration for variables that are never modified after declared
         "prefer-rest-params": 2, // suggest using the rest parameters instead of arguments
         "prefer-spread": 2, // suggest using the spread operator instead of .apply().
-        "prefer-template": 2, // suggest using template literals instead of strings concatenation
-
-        // babel plugin https://github.com/babel/eslint-plugin-babel
-        "babel/generator-star-spacing": 2, // Handles async/await functions correctly
-        "babel/new-cap": 0, // Ignores capitalized decorators (@Decorator)
-        "babel/array-bracket-spacing": 0, // Handles destructuring arrays with flow type in function parameters
-        "babel/object-curly-spacing": 0, // doesn't complain about export x from "mod"; or export * as x from "mod";
-        "babel/object-shorthand": 0, // doesn't fail when using object spread (...obj)
-        "babel/arrow-parens": 2, // Handles async functions correctly
-        "babel/no-await-in-loop": 1 // guard against awaiting async functions inside of a loop
+        "prefer-template": 2 // suggest using template literals instead of strings concatenation
     }
 };
